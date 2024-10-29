1feeb779cfc4a83ca7df0ce893135e1dcd7fe74a
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-10-29 20:07:58 +0800

    ifx: url load resource

--- django/mainapp/jinja/global_functions.py
+++ django/mainapp/jinja/global_functions.py
@@ -1,8 +1,8 @@
 import uuid
 
 def react_bundle_url(name):
-    return f'http://localhost:8080/{name}.dev.js?cachebust={uuid.uuid4()}'
+    return f'http://194.87.110.41:8080/{name}.dev.js?cachebust={uuid.uuid4()}'
 
 def css_url():
-    return f'http://localhost:8080/style.dev.css?cachebust={uuid.uuid4()}'
+    return f'http://194.87.110.41:8080/style.dev.css?cachebust={uuid.uuid4()}'
 
